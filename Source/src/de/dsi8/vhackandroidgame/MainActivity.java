75fcfc6660baecc16ba7216341f8b7a9a6b0bebf
Henrik Voß <hennevoss@gmail.com>
2012-09-15 11:22:29 +0200

    Added RoboGuice

--- Source/src/de/dsi8/vhackandroidgame/MainActivity.java
+++ Source/src/de/dsi8/vhackandroidgame/MainActivity.java
@@ -1,10 +1,10 @@
 package de.dsi8.vhackandroidgame;
 
+import roboguice.activity.RoboActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends RoboActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
